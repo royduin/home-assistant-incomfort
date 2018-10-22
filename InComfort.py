2e270d7db96918c50cf8ab670dd6cccd443adeac
Roy Duineveld <royduineveld@gmail.com>
2018-10-22 17:01:11 +0200

    Add 😎

---
+++ InComfort.py
@@ -0,0 +1,107 @@
+import json
+import logging
+
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+
+from urllib.request import urlopen
+from homeassistant.components.climate import (
+    STATE_HEAT, ClimateDevice, SUPPORT_TARGET_TEMPERATURE, PLATFORM_SCHEMA)
+from homeassistant.const import (
+    TEMP_CELSIUS, ATTR_TEMPERATURE)
+
+CONF_NAME = 'name'
+CONF_HOST = 'host'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_NAME): cv.string,
+})
+
+def _lsbmsb(lsb, msb):
+    return (lsb + msb*256) / 100.0
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the InComfort thermostat."""
+
+    name = config.get(CONF_NAME)
+    host = config.get(CONF_HOST)
+
+    add_devices([InComfortThermostat(name, host)])
+
+class InComfortThermostat(ClimateDevice):
+    """Representation of a InComfort Thermostat device."""
+
+    def __init__(self, name, host):
+        """Initialize the thermostat."""
+        self.host = host
+        self._name = name
+        self._current_temperature = None
+        self._target_temperature = None
+        self._state = None
+        self.data = None
+
+    @property
+    def state(self):
+        """Return the current state."""
+        if self.data is not None:
+            if bool(self.data['IO'] & 8):
+                self._state = 'Verwarmen'
+            else:
+                self._state = 'Standby'
+        else:
+            self._state = None
+        return self._state
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_TARGET_TEMPERATURE
+
+    @property
+    def name(self):
+        """Return the name of the thermostat, if any."""
+        return self._name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement which this thermostat uses."""
+        return TEMP_CELSIUS
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        if self.data is not None:
+            self._current_temperature = _lsbmsb(self.data['room_temp_1_lsb'], self.data['room_temp_1_msb'])
+        else:
+            self._current_temperature = None
+        return self._current_temperature
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        if self.data is not None:
+            self._target_temperature = _lsbmsb(self.data['room_set_ovr_1_lsb'], self.data['room_set_ovr_1_msb'])
+        else:
+            self._target_temperature = None
+        return self._target_temperature
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return True
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+
+        urlopen('http://' + self.host + '/data.json?heater=0&thermostat=0&setpoint=' + str((min(max(temperature, 5), 30) - 5.0) * 10))
+        self._target_temperature = temperature
+
+    def update(self):
+        """Get the latest data."""
+        response = urlopen('http://' + self.host + '/data.json?heater=0')
+        string = response.read().decode('utf-8')
+        self.data = json.loads(string)
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# home-assistant-incomfort
-Intergas InComfort integration with Home Assistant
+# Home Assistant InComfort
+
+Intergas InComfort integration with Home Assistant.
+
+- Feature request: https://community.home-assistant.io/t/intergas-incomfort-lan2rf-gateway/23967
+- Development talk: https://community.home-assistant.io/t/new-climate-thermostat-implementation-tips/74394
+
+## Installation
+
+Put the `InComfort.py` file in `custom_components/climate` within your configuration directory. On Ubuntu for example: `~/.homeassistant/custom_components/climate`. After that configure it in the `configuration.yaml` file:
+```
+climate:
+  - platform: InComfort
+    name: Woonkamer
+    host: 192.168.1.139
+```
+And change the `name` and `host` as wanted.
+
+## Why is it not in the core of Home Assistant?
+
+Because I'm not a Python developer. Can you get it there? As mentioned in [this](https://community.home-assistant.io/t/new-climate-thermostat-implementation-tips/74394) topic it should probably splitted into a seperated library where this integration talks with.
+
+## Ideas, bugs or suggestions?
+Please create a [issue](https://github.com/royduin/home-assistant-incomfort/issues) or a [pull request](https://github.com/royduin/home-assistant-incomfort/pulls).
+
+## License
+[MIT](LICENSE.md)
